98af1ce59f11f19cfb66040871363cae64e54866
JinYee Jeong <luvdo0112@daum.net>
2023-09-15 20:56:54 +0900

    springboot 기본 설정

---
+++ src/test/java/com/jolp/everyOUR/EveryOurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jolp.everyOUR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryOurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
